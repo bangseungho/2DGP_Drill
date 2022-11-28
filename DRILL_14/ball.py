75bec834d25b42ff5427c3eb93a3c41b351cd0ba
bangseungho <suengho2257@naver.com>
2022-11-28 13:13:08 +0900

    drill_14 완성

---
+++ DRILL_14/ball.py
@@ -0,0 +1,26 @@
+import random
+from pico2d import *
+import game_world
+import server
+
+class Ball:
+    image = None
+
+    def __init__(self):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y = random.randint(0, 1600), random.randint(0, 1200)
+
+    def draw(self):
+        self.sx, self.sy = self.x - server.background.window_left, self.y - server.background.window_bottom
+        self.image.draw(self.sx, self.sy)
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+    
+    def handle_collision(self, other, group):
+        game_world.remove_object(self)
+
